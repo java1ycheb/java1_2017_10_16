8f65cc3b43033997a5e567b61623505e7e09ecb1
student <student@tg6-12>
2017-10-23 20:19:19 +0300

    tf7

---
+++ MyFirstProject/src/ru/spec/java1/lec6/MapTest.java
@@ -0,0 +1,41 @@
+package ru.spec.java1.lec6;
+
+import java.security.KeyStore.SecretKeyEntry;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+public class MapTest {
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("hello", "world");
+		map.put("heo", "wld");
+		map.put("he", "word");
+		map.put("hlo", "rld");
+		map.put("helo", "wd");
+		System.out.println(map);
+		
+		
+		Set<Entry<String, String>> entrySet  = map.entrySet();
+		for (Entry<String, String> entry : entrySet) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+			System.out.println();
+		}
+		Set<String> keySet= map.keySet();
+		Collection<String> values = map.values();
+		
+		map.forEach((key,val) -> System.out.println(key +" -> "+ val));
+		//BiConsumer<String, String> consumer = new BiConsumer<String, String>() {
+
+		//	@Override
+		//	public void accept(String t, String u) {			
+		//	}
+		//};
+
+	}
+	
+
+}
--- MyFirstProject/src/ru/spec/java1/lec6/Palindrom.java
+++ MyFirstProject/src/ru/spec/java1/lec6/Palindrom.java
@@ -3,19 +3,45 @@ package ru.spec.java1.lec6;
 public class Palindrom {
 
 	public static void main(String[] args) {
-		for (int i = 0; i< 100000 ; i++) {
-			if (isPalendrom2(i,7) && isPalendrom2(i,17) ) {
-				System.out.println("("
-						+ i
-						+ ") "
-						+Long.toString(i,7)
-						+" "
-						+Long.toString(i,17));
-			}
+		//for (int i = 0; i< 100000 ; i++) {
+			//if (isPalendrom(i,7) && isPalendrom(i,17) ) {
+			//	System.out.println("("
+			//			+ i
+			//			+ ") "
+			//			+Long.toString(i,7)
+				//		+" "
+				//		+Long.toString(i,17));
+		//	}
+	//	}
+		for (int i = 0; i< 10 ; i++) {
+		test1();
+		test2();
+		test3();
 		}
 	}
+	private static void test1() {
+		long ts = System.nanoTime();
+		for (int i = 0; i< 10000000 ; i++) {
+			isPalen("asdfgqokjeklwqje");
+		}
+		System.out.println((System.nanoTime() - ts)/1000/1000d +"ms");
+	}
+	private static void test2() {
+		long ts = System.nanoTime();
+		for (int i = 0; i< 10000000 ; i++) {
+			isPalendrom2("asdfgqokjeklwqje");
+		}
+		System.out.println((System.nanoTime() - ts)/1000/1000d +"ms");
+	}
+	private static void test3() {
+		long ts = System.nanoTime();
+		for (int i = 0; i< 10000000 ; i++) {
+			isPalendrom3("asdfgqokjeklwqje");
+		}
+		System.out.println((System.nanoTime() - ts)/1000/1000d +"ms");
+	}
 
-	private static boolean isPalendrom(String str) {
+	private static boolean isPalen(String str) {
 		int len1 = str.length()/2;
 		for (int i = 0; i< len1 ; i++) {
 			int len2 =str.length() -1;
@@ -24,13 +50,29 @@ public class Palindrom {
 		return true;
 	}
 	
-	private static boolean isPalendrom2(long n, int r) {
+	private static boolean isPalendrom(long n, int r) {
 		String str;
 		str = Long.toString(n,r);		
 		
-	    return isPalendrom(str);
+	    return isPalen(str);
 	}
-	
+	private static boolean isPalendrom2(String str) {
+
+		char[] charArray = str.toCharArray();
+		int len1 = charArray.length/2;
+		
+		for (int i = 0; i< len1 ; i++) {
+			int len2 =charArray.length -1;
+			if ( charArray[i] != charArray[len2 - i]) return false;		
+		}
+		return true;
+	}
+	private static boolean isPalendrom3(String str) {
+		StringBuilder sb = new StringBuilder(str);
+		 			return	sb.reverse().equals(str);
+		
+		
+	}
 	
 	
 	
