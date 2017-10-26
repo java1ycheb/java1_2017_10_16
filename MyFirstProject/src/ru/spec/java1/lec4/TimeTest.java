ba7002cbc57f687205b6fe5298dbe8c4cdc4ec18
student <student@tg6-12>
2017-10-26 18:40:57 +0300

    tf8

--- MyFirstProject/src/ru/spec/java1/lec4/TimeTest.java
+++ MyFirstProject/src/ru/spec/java1/lec4/TimeTest.java
@@ -5,29 +5,22 @@ import java.util.Date;
 import java.util.LinkedList;
 
 public class TimeTest {
-	public static void main(String[] args) {
-		ArrayList<Integer>list = new ArrayList<>();
-		for (int i=0; i<1000_000;i++) {
-			list.add(i);
-		}
 
-		test();
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++)
+			test(); 
 
 	}
 
 	private static void test() {
-		
-		Integer[] array= new Integer[1000_000];
-		for (int i=0; i<1000_000;i++) {
-			array[i]=1;
-		}
-		long millis = System.currentTimeMillis();
-		long nano = System.nanoTime()/1000_000;
 		long ts = System.nanoTime();
-
-	//	long startTime = millis-nano;
-	//	System.out.println(new Date(startTime ));
-	//	System.out.println(System.currentTimeMillis() - millis);
-		System.out.println((System.nanoTime() - ts)/1000/1000d +"ms");
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i=0 ; i < 100_000; i++) {
+			list.add(list.size()/2,i);
+		}
+//		System.out.println(System.currentTimeMillis() - millis);
+		System.out.println((System.nanoTime() - ts)/1000/1000d+"ms "+list.size());
 	}
+
 }
